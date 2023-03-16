f86383f087fdafee4ac7ca37ccf26ec10ecc7ccb
MJCappella <mjaymickey001@gmail.com>
2023-03-16 04:21:03 -0700

    low level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Function: main - Prints out a line  to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
