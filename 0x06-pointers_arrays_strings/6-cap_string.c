1b1548034b687000094fd05ac6b41b29d4d2a990
MICHAEL JAMES <97917001+MJCappella@users.noreply.github.com>
2023-03-30 23:03:07 +0300

    updated whole file
    Signed-off-by: MICHAEL JAMES <97917001+MJCappella@users.noreply.github.com>

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,36 @@
 #include "main.h"
-#include<stdio.h>
-
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
  *
- * Return: A pointer to the converted string.
+ *Return: pointer to s.
  */
-int main(void){
-char *cap_string(char *str)
-	int word = 0;
+char *cap_string(char *s)
+{
+	int count;
 
-	while (str[word])
-	{
-		while (!(str[word] >= 'a' && str[word] <= 'z'))
-			word++;
-
-		if (str[word - 1] == ' ' ||
-		    str[word - 1] == '\t' ||
-		    str[word - 1] == '\n' ||
-		    str[word - 1] == ',' ||
-		    str[word - 1] == ';' ||
-		    str[word - 1] == '.' ||
-		    str[word - 1] == '!' ||
-		    str[word - 1] == '?' ||
-		    str[word - 1] == '"' ||
-		    str[word - 1] == '(' ||
-		    str[word - 1] == ')' ||
-		    str[word - 1] == '{' ||
-		    str[word - 1] == '}' ||
-		    index == 0)
-			str[word] -= 32;
-
-		word++;
+/*  scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
-
-	return (str);
+	return (s);
 }
